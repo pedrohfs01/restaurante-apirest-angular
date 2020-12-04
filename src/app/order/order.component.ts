41152a6e02a52c8b3a847d203d65240cdb6cb1b0
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-04 11:23:41 -0300

    Implementar componente de total da compra

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -9,6 +9,8 @@ import { OrderService } from 'app/services/order.service';
 })
 export class OrderComponent implements OnInit {
 
+  delivery: number = 10;
+
   paymentOptions: RadioOption[] = [
     { label: "Dinheiro", value: "MON" },
     { label: "Cartão de débito", value: "DEB" },
@@ -18,6 +20,10 @@ export class OrderComponent implements OnInit {
   constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number{
+    return this.orderService.itemsValue();
   }
 
   cartItems() {
