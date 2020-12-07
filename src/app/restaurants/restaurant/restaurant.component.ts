51de7fe2bb8a6365dc8b02c707bad2049b6dae1b
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-07 11:16:30 -0300

    Criar transição na página de restaurantes

--- src/app/restaurants/restaurant/restaurant.component.ts
+++ src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,11 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition  } from '@angular/animations';
 import { Restaurant } from 'app/models/restaurant.model';
 
 @Component({
   selector: 'mt-restaurant',
-  templateUrl: './restaurant.component.html'
+  templateUrl: './restaurant.component.html',
+  animations: [
+    trigger('restaurantAppeared', [
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [
+        style({
+          opacity: 0, transform: 'translate(30-px, -10px)'
+        }), animate('500ms 0s ease-in-out')
+      ])
+    ])
+  ]
 })
 export class RestaurantComponent implements OnInit {
+
+  restaurantState = 'ready'
 
   @Input() restaurant: Restaurant;
 
