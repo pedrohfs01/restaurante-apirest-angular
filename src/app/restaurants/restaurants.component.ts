b315e8867d1ccacce7102f8ac473cb144d3c911c
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-07 13:55:05 -0300

    Adicionar barra de busca

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Restaurant } from 'app/models/restaurant.model';
 import { RestauranteService } from 'app/services/restaurante.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+
 
 @Component({
   selector: 'mt-restaurants',
-  templateUrl: './restaurants.component.html'
+  templateUrl: './restaurants.component.html',
+  animations: [
+    trigger('toggleSearch', [
+      state('hidden', style({
+        opacity: 0,
+        "max-height": "0px"
+      })),
+      state('visible', style({
+        opacity: 1,
+        "max-height": "70px",
+        "margin-top": "20px"
+      })),
+      transition('* => *', animate('250ms 0s ease-in-out'))
+    ])
+  ]
 })
 export class RestaurantsComponent implements OnInit {
+
+  searchBarState = 'hidden'
 
   restaurants: Restaurant[];
 
@@ -16,4 +34,7 @@ export class RestaurantsComponent implements OnInit {
     this.restaurantService.restaurants().subscribe(response => this.restaurants = response);
   }
 
+  toggleSearch(){
+    this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden';
+  }
 }
