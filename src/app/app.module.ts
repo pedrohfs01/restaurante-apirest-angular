37cc489a06707b4466810eeb65233b159cbca78a
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-07 09:25:12 -0300

    Módulos personalizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
-
 import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-
-import { ROUTES } from './app.routes';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestauranteService } from './services/restaurante.service';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './services/shopping-cart.service';
-import { OrderComponent } from './order/order.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItensComponent } from './order/order-itens/order-itens.component';
-import { OrderService } from './services/order.service';
-import { DeliveryCostComponent } from './order/delivery-cost/delivery-cost.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
-import { RatingComponent } from './shared/rating/rating.component';
+import { DeliveryCostComponent } from './order/delivery-cost/delivery-cost.component';
+import { OrderItensComponent } from './order/order-itens/order-itens.component';
+import { OrderComponent } from './order/order.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { OrderService } from './services/order.service';
+import { RestauranteService } from './services/restaurante.service';
+import { ShoppingCartService } from './services/shopping-cart.service';
+import { SharedModule } from './shared/shared.module';
+
+
+
 
 
 @NgModule({
@@ -42,19 +39,15 @@ import { RatingComponent } from './shared/rating/rating.component';
     MenuItemComponent,
     ReviewsComponent,
     OrderComponent,
-    InputComponent,
-    RadioComponent,
     OrderItensComponent,
     DeliveryCostComponent,
-    OrderSummaryComponent,
-    RatingComponent
+    OrderSummaryComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RouterModule.forRoot(ROUTES),
-    FormsModule,
-    ReactiveFormsModule
+    SharedModule
   ],
   providers: [RestauranteService,
               ShoppingCartService,
