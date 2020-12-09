4d4c548cfe79b4d688089efabf28b2285d92a660
Pedro Henrique Fernandes Santos <pedexg@gmail.com>
2020-12-09 11:24:43 -0300

    Zonear notificações de erro

--- src/app/exceptions/error-handler.ts
+++ src/app/exceptions/error-handler.ts
@@ -1,33 +1,36 @@
 import { HttpErrorResponse } from "@angular/common/http";
-import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { ErrorHandler, Injectable, Injector, NgZone } from "@angular/core";
 import { LoginService } from "app/services/login.service";
 import { NotificationService } from "app/services/notification.service";
 import "rxjs/add/observable/throw";
 
 @Injectable()
 export class ApplicationErrorHandler extends ErrorHandler {
-    
-    constructor(private ns: NotificationService, private injector: Injector){
+
+    constructor(private ns: NotificationService,
+        private injector: Injector,
+        private zone: NgZone) {
         super();
     }
 
     errorHandler(errorResponse: HttpErrorResponse | any) {
-        if(errorResponse instanceof HttpErrorResponse){
+        if (errorResponse instanceof HttpErrorResponse) {
             const message = errorResponse.error.message;
-        
-            switch(errorResponse.status){
-                case 401:
-                    this.injector.get(LoginService).handleLogin();
-                    break;
-                case 403:
-                    this.ns.notify(message || 'Não autorizado.')
-                    break;
-                case 404:
-                    this.ns.notify(message || 'Recurso não encontrado.')
-                    break;
-            }
+            this.zone.run(() => {
+                switch (errorResponse.status) {
+                    case 401:
+                        this.injector.get(LoginService).handleLogin();
+                        break;
+                    case 403:
+                        this.ns.notify(message || 'Não autorizado.')
+                        break;
+                    case 404:
+                        this.ns.notify(message || 'Recurso não encontrado.')
+                        break;
+                }
+            });
         }
-        
+
         super.handleError(errorResponse);
     }
 }
